41e9af8d08de3bc55fd6d81cfb7dac54c4b898a8
wflixu <wflixu@88.com>
2024-01-24 23:30:02 +0800

    Initial Commit

---
+++ ZhaiHuiJi/ZhaiHuiJiApp.swift
@@ -0,0 +1,32 @@
+//
+//  ZhaiHuiJiApp.swift
+//  ZhaiHuiJi
+//
+//  Created by 李旭 on 2024/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ZhaiHuiJiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
